413df1c58055ac87e448a87930d420de6bf44c59
ashok <korakotiashok@gmail.com>
2021-09-01 17:12:57 +0530

    added comments

--- OLX-EurekaServerApplication/src/main/java/com/spring/eureka/EurekaServerApplication.java
+++ OLX-EurekaServerApplication/src/main/java/com/spring/eureka/EurekaServerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+//application starts from here
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
